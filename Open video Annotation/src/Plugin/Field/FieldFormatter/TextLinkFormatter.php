437873bdac832bfaf8eb256d719527144b036d9f
pravinkumar <pravinkumar@unimity.com>
2016-06-30 09:00:09 +0530

    coding standard update

--- Open video Annotation/src/Plugin/Field/FieldFormatter/TextLinkFormatter.php
+++ Open video Annotation/src/Plugin/Field/FieldFormatter/TextLinkFormatter.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\open_video_annotation\Plugin\Field\FieldFormatter\TextLinkFormatter.
- */
-
 namespace Drupal\open_video_annotation\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
@@ -22,25 +17,25 @@ use Drupal\Core\Field\FormatterBase;
  *   }
  * )
  */
-
 class TextLinkFormatter extends FormatterBase {
 
-/**
- * {@inheritdoc}
- */
-public function viewElements(FieldItemListInterface $items, $langcode) {
-  
-  $elements = array();
-  foreach ($items as $delta => $item) {
-    $get_uri = $item->getValue();
-    // check url is from textfield
-    if (array_key_exists('value', $get_uri)) {
-      $elements[$delta] = array(
-        '#theme' => 'text_link_formatter',
-        '#url' => $get_uri['value'],
-      );
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+
+    $elements = array();
+    foreach ($items as $delta => $item) {
+      $get_uri = $item->getValue();
+      // Check url is from textfield.
+      if (array_key_exists('value', $get_uri)) {
+        $elements[$delta] = array(
+          '#theme' => 'text_link_formatter',
+          '#url' => $get_uri['value'],
+        );
+      }
     }
+    return $elements;
   }
-  return $elements;
-  }
+
 }
